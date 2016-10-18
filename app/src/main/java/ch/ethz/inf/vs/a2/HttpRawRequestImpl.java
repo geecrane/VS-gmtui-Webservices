d22111a2b58312db1b0a30bc0aa3fb49ce58aeee
George Mtui <gmtui@student.ethz.ch>
2016-10-18 15:43:41 +0200

    Create Skeleton for Task 1

---
+++ app/src/main/java/ch/ethz/inf/vs/a2/HttpRawRequestImpl.java
@@ -0,0 +1,15 @@
+package ch.ethz.inf.vs.a2;
+
+
+import ch.ethz.inf.vs.a2.http.HttpRawRequest;
+
+/**
+ * Created by george on 18.10.16.
+ */
+
+public class HttpRawRequestImpl implements HttpRawRequest {
+    @Override
+    public String generateRequest(String host, int port, String path) {
+        return null;
+    }
+}
---
+++ app/src/main/java/ch/ethz/inf/vs/a2/TextSensor.java
@@ -0,0 +1,19 @@
+package ch.ethz.inf.vs.a2;
+
+import ch.ethz.inf.vs.a2.sensor.AbstractSensor;
+
+/**
+ * Created by george on 18.10.16.
+ */
+
+public class TextSensor extends AbstractSensor {
+    @Override
+    public String executeRequest() throws Exception {
+        return null;
+    }
+
+    @Override
+    public double parseResponse(String response) {
+        return 0;
+    }
+}
